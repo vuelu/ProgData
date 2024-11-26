129dfc4c179bdfdd5728f8c5a6bd10f550e0fe9f
vuelu <134621235+vuelu@users.noreply.github.com>
2024-11-26 01:50:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000;
 
-server.use(cors());
 
 server.use(middlewares);
 server.use(router);
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+server.listen(port);
